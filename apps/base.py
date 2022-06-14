8e07a56eeb8f8c1e141d88b47a3b9a30ba25d0a2
night_crow <night_crow@qq.com>
2022-06-14 21:02:38 +0800

    update

--- apps/base.py
+++ apps/base.py
@@ -1,11 +1,15 @@
+import os
+
 from flask import Flask
 
 from apps.configs import Config
 
+static_path = os.path.join(os.path.dirname(__file__), 'static')
+
 
 def create_app():
     config = Config
-    app = Flask(config.SERVICE_NAME, static_url_path='/static')
+    app = Flask(config.SERVICE_NAME, static_folder=static_path)
     app.config.from_object(config)
 
     return app
